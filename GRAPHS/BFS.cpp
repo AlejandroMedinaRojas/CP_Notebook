e7985911ee2d82b5ba80ecb72233472b24477da4
Alejandro Medina <alejandromedinarojas05@gmail.com>
2025-05-30 15:51:01 -0500

    Updated BFS

--- GRAPHS/BFS.cpp
+++ GRAPHS/BFS.cpp
@@ -2,32 +2,37 @@
 
 using namespace std;
 
-int n;
-vector<vector<int>> graph(n);
-vector<bool> visited(n, false);
-vector<int> distances(n, 0);
+int main(){
 
-vector<int> BFS(vector<int> firsts){
+    int n;
+    vector<vector<int>> adjs(n);        // Lista de adyacencia
+    vector<bool> visited(n, false);     // Lista de nodos visitados
+    vector<int> distances(n, 0);        // Distancia a los nodos
+
     queue<int> qu;
+    vector<int> firsts;                         // Inicializar los elementos por los que empezará la BFS
     for (int i = 0; i < firsts.size(); ++i){
         visited[firsts[i]] = true;
         qu.push(firsts[i]);
     }
     int currsize = qu.size(), currdis = 1, curr = qu.front();
-    while (currsize){
+
+    function<void(vector<int>)> BFS = [&](vector<int> firsts){
         while (currsize){
-            curr = qu.front();
-            for (int i = 0; i < graph[curr].size(); ++i){
-                if (!visited[graph[curr][i]]){
-                    qu.push(graph[curr][i]);
-                    distances[graph[curr][i]] = currdis;
-                    visited[graph[curr][i]] = true;
+            while (currsize){
+                curr = qu.front();
+                for (int i = 0; i < adjs[curr].size(); ++i){
+                    if (!visited[adjs[curr][i]]){
+                        qu.push(adjs[curr][i]);
+                        distances[adjs[curr][i]] = currdis;
+                        visited[adjs[curr][i]] = true;
+                    }
                 }
+                --currsize;
+                qu.pop();
             }
-            --currsize;
-            qu.pop();
+            currsize = qu.size();
+            ++currdis;
         }
-        currsize = qu.size();
-        ++currdis;
-    }
+    };
 }
